e701c9504765c1f72dcf75c9a24a39ef4faa6df3
Joseph Petrasek <jpetrasek@evertz.com>
2025-05-22 14:38:14 -0400

    removing spliting

--- src/app/track-table/track.types.ts
+++ src/app/track-table/track.types.ts
@@ -23,7 +23,6 @@ export interface TrackRow {
 }
 
 export interface ExtendedTrackRow extends TrackRow {
-    splitCount: number;  // Making splitCount required with initial value of 0
     isParentRow?: boolean;  // Indicates if this is a parent row that can be split
     parentRowIndex?: number;  // Reference to the parent row if this is a split row
 }
